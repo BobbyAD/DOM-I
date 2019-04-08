9d2e4cd174397161b913ca32942e030d1f1cf19f
Robert Driskell <driskell.ba@gmail.com>
2019-04-08 15:34:15 -0500

    Got everything down to Bottom Content put in

--- js/index.js
+++ js/index.js
@@ -38,5 +38,65 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+//Nav
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//THIS IS HACKY GARBAGE
+let nav = document.querySelectorAll('header nav a');
+for (let i = 0; i < 6; i++) {
+  var n = i + 1;
+  nav[i].textContent = siteContent['nav']['nav-item-' + n.toString()];
+};
+
+
+//CTA
+let ctaText = document.querySelector('.cta-text h1');
+ctaText.textContent = siteContent['cta']['h1'];
+
+let ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = siteContent['cta']['button'];
+
+let ctaImg = document.querySelector('#cta-img');
+ctaImg.setAttribute('src', siteContent['cta']['img-src']);
+
+
+//TOP CONTENT
+let featuresHeader = document.querySelector('.top-content .text-content h4');
+featuresHeader.textContent = siteContent['main-content']['features-h4'];
+
+let featuresText = document.querySelector('.top-content .text-content p');
+featuresText.textContent = siteContent['main-content']['features-content'];
+
+let aboutHeader = document.querySelectorAll('.top-content .text-content h4')[1];
+aboutHeader.textContent = siteContent['main-content']['about-h4'];
+
+let aboutText = document.querySelectorAll('.top-content .text-content p')[1];
+aboutText.textContent = siteContent['main-content']['features-content'];
+
+
+//MIDDLE IMG
+let middleImg = document.querySelector('.middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+
+//BOTTOM CONTENT
+let servicesHeader = document.querySelector('.bottom-content .text-content h4');
+servicesHeader.textContent = siteContent['main-content']['services-h4'];
+
+let servicesText = document.querySelector('.bottom-content .text-content p');
+servicesText.textContent = siteContent['main-content']['services-content'];
+
+let productHeader = document.querySelectorAll('.bottom-content .text-content h4')[1];
+productHeader.textContent = siteContent['main-content']['product-h4'];
+
+let productText = document.querySelectorAll('.bottom-content .text-content p')[1];
+productText.textContent = siteContent['main-content']['product-content'];
+
+let visionHeader = document.querySelectorAll('.bottom-content .text-content h4')[2];
+visionHeader.textContent = siteContent['main-content']['vision-h4'];
+
+let visionText = document.querySelectorAll('.bottom-content .text-content p')[2];
+visionText.textContent = siteContent['main-content']['vision-content'];
+
+//CONTACT
